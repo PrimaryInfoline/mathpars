a0b86b7dc5b6054949f75bb3d574663c1b4a41fa
simas <simas@DESKTOP-O4K62N7>
2022-12-05 02:23:55 +0300

    Добавьте файлы проекта.

---
+++ mathpars.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+
+int main()
+{
+
+    std::string math_exp = "a+(b+47*(g/78))-4878/(15*h)=j";
+    std::cout << math_exp << std::endl;
+
+    std::regex regular("[(][-]?(([\\d]+|[a-z])[-+*/])+([\\d]+|[a-z])[)]");
+    std::regex exp("[-]?(([\\d]+|[a-z])[-+*/])+([\\d]+|[a-z])[=][-]?([\\d]+|[a-z])");
+
+    while (std::regex_search(math_exp.c_str(), regular))
+    {
+        math_exp = regex_replace(math_exp.c_str(), regular, "c");
+        std::cout << math_exp << std::endl;
+    }
+    
+    if (std::regex_match(math_exp.c_str(), exp))
+    {
+        std::cout << "correct\n";
+    }
+    else
+    {
+        std::cout << "incorrect\n";
+    }
+}
